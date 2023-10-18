0fa930c50a12867f54d45115c818d4187b669f05
lenin254 <leninxarebalo18@hotmail.com>
2023-10-18 10:24:35 -0500

    Parcial terminado

---
+++ todo_app/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :stories
+  resources :todos
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  get '/hello', to: 'todos#hello'
+end
